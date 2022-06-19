c2363d4b31d35502df7a71bb214cd8f344029c36
unknown <altabam@yahoo.com.ar>
2022-06-19 00:20:43 -0300

    primer crud Hortelanos que se guarda con datos

--- code/configuracion/urls.py
+++ code/configuracion/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns= [ 
         path('', views.index,name='index'),
         path('<int:persona_id>/', views.consultaPersona, name='consultaPersona'),
+        path('registrarHortelano/', views.registrarHortelano),
+        path('editarHortelano/<int:id>/', views.editarHortelano),
+       path('modificarHortelano/', views.modificarHortelano),
+       path('eliminarHortelano/<int:id>/', views.eliminarHortelano),
+       
 ]
