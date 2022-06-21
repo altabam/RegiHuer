85a0d94d6ce6c8848a657777f22a014f9bb7e9e9
altabam <altabam@yahoo.com.ar>
2022-06-21 11:10:14 -0300

    subida de de cambios en modelos Huerta

--- code/configuracion/views.py
+++ code/configuracion/views.py
@@ -1,12 +1,19 @@
 from urllib import response, request
 from django.shortcuts import render
-
+from django.template import loader
+from .models import Hortelano
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hola Mundo, Bienvenidos a la app Registro de Huertas ")
-
+    listaHortelanos = Hortelano.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'listaHortelanos': listaHortelanos,
+    }
+    return HttpResponse(template.render(context, request))
+    
+    
 def consultaPersona(request, persona_id):
     return HttpResponse("Estas mirando la persona: %s." % persona_id )
--- code/regihuer/settings.py
+++ code/regihuer/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'regihuer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./RegiHuer/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
