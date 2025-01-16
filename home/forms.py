75573952beac084f52a9e10ca8b84fdc7b3804f9
Marcelo <altabam@yahoo.com.ar>
2025-01-16 18:10:06 -0300

    creacion de usuario temporal para registro

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UsuarioPromesa
 
 class RegistracionUsuarioForm(UserCreationForm):
+    username = forms.CharField(label="Usuario")
     email = forms.EmailField()
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2 = forms.CharField(label="ConfirmaContraseña", widget=forms.PasswordInput)
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
 
     class meta:
         model = User
-        fields= ['username', 'email','password1','password2']
+        fields= ['first_name','last_name','username', 'email','password1','password2']
+        help_texts ={k:"" for k in fields}
+
+class UsuarioPromesaForm(forms.ModelForm):
+    username = forms.CharField(label="Usuario")
+    email = forms.EmailField()
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
+    class Meta:
+        model = UsuarioPromesa
+#        fields = '__all__'
+        fields= ['first_name','last_name','username', 'email']
         help_texts ={k:"" for k in fields}
--- home/views.py
+++ home/views.py
@@ -3,37 +3,38 @@ from django.contrib import messages
 from django.core.mail import EmailMessage
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.template.loader import render_to_string
-
-from .forms import RegistracionUsuarioForm
-
+import hashlib, datetime, random, os
+from .models import UsuarioPromesa
+from .forms import RegistracionUsuarioForm, UsuarioPromesaForm
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def registrar(request):
+    #up = UsuarioPromesa.objects.all().delete()
     if request.method=='POST':
-        form = RegistracionUsuarioForm(request.POST)
+        form = UsuarioPromesaForm(request.POST)
         print("no si es valido")
         if form.is_valid():
+            usuarioPromesa = generarUsuarioPromesa(form)
             username = form.cleaned_data['username']
             messages.success(request, f'Usuario {username} creado')
-            print("paso por form valido")
-            enviarEmail(form)
+            enviarEmail(usuarioPromesa)
             return render(request, "index.html")
     else:
-        form = RegistracionUsuarioForm()
+        form = UsuarioPromesaForm()
     contexto = {'form': form,}
     return render(request, "registration/registracion.html", contexto)
 
-def enviarEmail(form):
-    token_generator = PasswordResetTokenGenerator()
-    token = token_generator.make_token(form)
-    username = form.cleaned_data['username']
+def enviarEmail(up):
     
     # Renderizar la plantilla
-    url_app = "https://"+ "/administracion/mailValidador"
+    url_app = "https://"+os.getenv("URL_APP")+ "/administracion/mailValidador?tk="+up.token
     print(url_app)
-    mensaje = render_to_string("templateEmail.html",{'toke':token, 'username':username})    # Enviar el correo
+    print(up.fecha)
+    un = up.username
+    mensaje = render_to_string("registration/templateEmail.html",{'username':un, 'url_app':url_app})    # Enviar el correo
+    print (mensaje)
     email = EmailMessage(
       subject='Bienvenido a nuestro sitio',
       body=mensaje,
@@ -42,5 +43,20 @@ def enviarEmail(form):
     )
     
     email.content_subtype = 'html'  # Define el tipo como HTML
-    email.send()
-  
+   # email.send()
+def generarUsuarioPromesa(form):
+    form.save()
+    un = form.cleaned_data['username']
+    up = UsuarioPromesa.objects.get(username = un)
+    alea = str(random.random())
+    text = up.first_name + alea + up.username +str(up.fecha) + up.email + str(up.pk) + up.last_name 
+    print(text)
+
+    up.token = hashlib.sha256(text.encode()).hexdigest()
+    up.aleatorio = alea
+    print(up.token)
+    print(up.aleatorio)
+    up.save()
+    #token = token_generator.make_token(form)
+    
+    return up  
